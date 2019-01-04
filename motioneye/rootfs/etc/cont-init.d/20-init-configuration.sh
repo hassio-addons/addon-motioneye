97520a58770fd2abee6cc8e780a5bc27120929a9
ludeeus <joasoe@gmail.com>
2019-01-04 14:03:24 +0100

    ✨Adds motion webcontrol

--- motioneye/rootfs/etc/cont-init.d/20-init-configuration.sh
+++ motioneye/rootfs/etc/cont-init.d/20-init-configuration.sh
@@ -10,3 +10,12 @@ if ! hass.directory_exists '/data/motioneye'; then
     cp -R /etc/motioneye /data/motioneye \
         || hass.die 'Failed to create initial motionEye configuration'
 fi
+
+if hass.config.true 'motion_api'; then
+    hass.log.info "Enabling motion webcontrol"
+    sed -i "s/motion_control_localhost true/motion_control_localhost false/" \
+        "/data/motioneye/motioneye.conf"
+else
+    sed -i "s/motion_control_localhost flase/motion_control_localhost true/" \
+        "/data/motioneye/motioneye.conf"
+fi
