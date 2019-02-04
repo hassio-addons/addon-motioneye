8c2a8f9f7df898da19beffa366852a1cf28010f3
Franck Nijhof <frenck@frenck.nl>
2019-02-04 21:10:26 +0100

    :warning: Added warning logs

--- motioneye/rootfs/etc/cont-init.d/31-motion.sh
+++ motioneye/rootfs/etc/cont-init.d/31-motion.sh
@@ -9,7 +9,10 @@ source /usr/lib/hassio-addons/base.sh
 readonly motion='/data/motioneye/motion.conf'
 
 if hass.config.true 'motion_webcontrol'; then
-    hass.log.info "Enabling motion webcontrol"
+    hass.log.info "Enabling motion webcontrol..."
+    hass.log.warning "This opens up an UNSECURE port to the outside world!"
+    hass.log.warning "This port has NO SSL & NO AUTHENICATION!"
+    hass.log.warning "YOU HAVE BEEN WARNED!"
     sed -i "s/webcontrol_localhost on/webcontrol_localhost off/" "$motion"
 else
     sed -i "s/webcontrol_localhost off/webcontrol_localhost on/" "$motion"
