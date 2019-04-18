9c5d79c63e4ec807b4ee7b49f2f2074565d538a2
Franck Nijhof <frenck@addons.community>
2019-04-18 22:13:11 +0200

    :hammer: Merged init scripts

--- motioneye/rootfs/etc/cont-init.d/motioneye.sh
+++ motioneye/rootfs/etc/cont-init.d/motioneye.sh
@@ -4,6 +4,7 @@
 # Creates initial motionEye configuration in case it is non-existing
 # ==============================================================================
 readonly CONF='/data/motioneye/motioneye.conf'
+readonly MOTION='/data/motioneye/motion.conf'
 
 if ! bashio::fs.directory_exists '/data/motioneye'; then
     cp -R /etc/motioneye/ /data/motioneye/ \
@@ -26,4 +27,21 @@ if bashio::fs.file_exists "${CONF}"; then
         -name "thread-*.conf" \
         -exec \
             /usr/lib/python2.7/site-packages/motioneye/scripts/migrateconf.sh {} \;
+fi
+
+# Configure motion webcontrol access
+if bashio::config.true 'motion_webcontrol'; then
+    bashio::log.info "Enabling motion webcontrol..."
+    bashio::.log.warning "This opens up an UNSECURE port to the outside world!"
+    bashio::.log.warning "This port has NO SSL & NO AUTHENICATION!"
+    bashio::.log.warning "YOU HAVE BEEN WARNED!"
+    sed -i "s/webcontrol_localhost on/webcontrol_localhost off/" "$MOTION"
+else
+    sed -i "s/webcontrol_localhost off/webcontrol_localhost on/" "$MOTION"
+fi
+
+# Creates initial motionEye media folder in case it is non-existing
+if ! bashio::fs.directory_exists '/share/motioneye'; then
+    mkdir -p /share/motioneye \
+        || bashio::exit.nok 'Failed to create initial motionEye media folder'
 fi
